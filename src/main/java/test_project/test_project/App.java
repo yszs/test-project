68a2b01b4d5eed7e7eea59ff9af4fdec997f1833
zhengz <zhengz@bsoft.com.cn>
2015-04-01 15:46:41 +0800

    ss

--- src/main/java/test_project/test_project/App.java
+++ src/main/java/test_project/test_project/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.999999println( "Hello World!" );
+	\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
     }
 }
