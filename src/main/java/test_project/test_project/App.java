0cf0e9d215fd156514f5d2ac71661d50ab7c0897
zhengz <zhengz@bsoft.com.cn>
2015-04-01 14:24:18 +0800

    create project

---
+++ src/main/java/test_project/test_project/App.java
@@ -0,0 +1,13 @@
+package test_project.test_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
